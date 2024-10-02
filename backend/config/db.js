9a26b901934efe272d75e32061738a9721371109
madhan <madhanb687@gmail.com>
2024-10-02 18:48:24 +0530

    db

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb://localhost:27017').then(()=>console.log("DataBase Connected"));
+    await mongoose.connect('mongodb://localhost:27017/merndb').then(()=>console.log("DataBase Connected"));
 }
