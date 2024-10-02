24ec2e514c6a8b02faef270e3b3c91246c492158
madhan <madhanb687@gmail.com>
2024-10-02 19:11:39 +0530

    sd

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb://localhost:27017/mydb').then(()=>console.log("DataBase Connected"));
+    await mongoose.connect('mongodb://localhost:27017/mydb',{ useNewUrlParser: true, useUnifiedTopology: true }).
+    then(()=>console.log("DataBase Connected")).catch((error)=>{
+        console.log('db not connected');
+    });
 }
