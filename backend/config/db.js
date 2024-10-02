b4cd09941098e52be9bab0dfef7087c8f77567ef
madhan <madhanb687@gmail.com>
2024-10-02 22:03:41 +0530

    db change

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb://localhost:27017/mern',{ useNewUrlParser: true, useUnifiedTopology: true }).
+    await mongoose.connect('mongodb+srv://madhan91101:Mcabca%409@klncollege.ab2hmvj.mongodb.net/mern',{ useNewUrlParser: true, useUnifiedTopology: true }).
     then(()=>{console.log("DataBase Connected")}).catch((error)=>{
         console.log('db not connected');
     });
