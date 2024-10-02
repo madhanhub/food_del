6c38d923c8de556dcc982c462cf338dec3d3eaae
madhan <madhanb687@gmail.com>
2024-10-02 15:06:09 +0530

    mongo

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('// UR DATABASE WITH ATLAS MONGODB').then(()=>console.log("DataBase Connected"));
+    await mongoose.connect('mongodb://localhost:27017').then(()=>console.log("DataBase Connected"));
 }
