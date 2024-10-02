1dad144780fd4e9568625816d0c7b31e3347cd02
madhan <madhanb687@gmail.com>
2024-10-02 19:15:14 +0530

    mern

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb://localhost:27017/mydb',{ useNewUrlParser: true, useUnifiedTopology: true }).
+    await mongoose.connect('mongodb://localhost:27017/mern',{ useNewUrlParser: true, useUnifiedTopology: true }).
     then(()=>console.log("DataBase Connected")).catch((error)=>{
         console.log('db not connected');
     });
