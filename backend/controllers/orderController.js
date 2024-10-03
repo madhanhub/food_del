0d769502fb77dccfe077d01617b80d74002cbd95
madhan <madhanb687@gmail.com>
2024-10-03 13:05:05 +0530

    hasgd

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY)
 // placing user order from frontend
 const placeOrder = async (req,res) => {
 
-    const frontend_url = "https://food-del-frontend-lpdm.onrender.com";
+    const frontend_url = "https://food-del-frontend-2617.onrender.com";
 
     try {
         const newOrder = new orderModel({
