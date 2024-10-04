51cb874fdab71edb4f6b539990d3540d6ea6bdf4
madhan <madhanb687@gmail.com>
2024-10-04 12:21:40 +0530

    hg

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Sidebar from './components/Sidebar/Sidebar'
-
 import { Routes,Route } from 'react-router-dom'
 import Add from './pages/Add/Add'
 import List from './pages/List/List'
 import Orders from './pages/Orders/Orders'
-import EditFood from './pages/EditFood/EditFood'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
